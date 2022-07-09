26792db5cdc85a31d37385ff657e807bb02fc26f
verhovenskyivan <verhovenskyivan@gmail.com>
2022-07-09 19:40:21 +0300

    First ASP.NET app

---
+++ ASP.NET_WEB_APP/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ASP.NET_WEB_APP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NET_WEB_APP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IActionResult PrintInfo()
+        {
+            return View();
+        }
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var adminName = Configuration.GetSection("Admin:Name");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
